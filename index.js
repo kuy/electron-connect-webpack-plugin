18606534260b817d3a98da9fa9800b94240480ee
底なし沼の魔女 <reina07@gmail.com>
2016-12-07 20:58:28 +0900

    Update index.js
    newした際にオプションを渡せる用に追加

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 var electron = require('electron-connect').server;
 
 function ElectronConnectWebpackPlugin(options) {
+  ElectronConnectWebpackPlugin.prototype.options = options;
 }
 
 ElectronConnectWebpackPlugin.prototype.apply = function(compiler) {
   var server;
+  var self = this;
 
   function restart() {
     if (typeof server === 'undefined') {
-      server = electron.create();
+      server = electron.create(self.options);
       server.start();
     } else {
       server.restart();
